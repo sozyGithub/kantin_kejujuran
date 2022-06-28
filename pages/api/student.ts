50654953432b9f073a0c63fcc7df4177a8fba412
sozy <thinpotatogithub@gmail.com>
2022-06-29 01:20:16 +0800

    fix income bug

--- pages/api/student.ts
+++ pages/api/student.ts
@@ -59,8 +59,8 @@ export default async function handler(
           student_id,
         },
         data: {
-          balance,
-          income,
+          balance: balance,
+          income: income,
         },
       });
       res
